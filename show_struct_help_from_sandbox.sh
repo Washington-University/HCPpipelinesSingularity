a35e80fad009b787361faaf065cef5bddc01cba5
Timothy B. Brown <tbbrown@wustl.edu>
2018-08-30 15:17:48 -0500

    General improvements to utility scripts.

--- show_struct_help_from_sandbox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This utility script will show you the help provided by the Structural Preprocessing app
-# within the HCPprocessingPipelinesSandbox
-
-time singularity exec HCPprocessPipelinesSandbox/ /pipeline_tools/xnat_pbs_jobs/StructuralPreprocessing/StructuralPreprocessing.SINGULARITY_PROCESS --help
-
